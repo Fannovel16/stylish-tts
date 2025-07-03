3f4612cef150dc5af1ff04ab3eef7090e31100b3
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-03 20:57:55 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/stage_train.py
+++ train/stylish_train/stage_train.py
@@ -79,20 +79,12 @@ def train_textual(
         train.stage.optimizer.zero_grad()
         log = build_loss_log(train)
         train.stft_loss(pred.audio.squeeze(1), batch.audio_gt, log)
-        print(
-            state.phones.shape,
-            F.cosine_similarity(
-                state.phones_prediction.transpose(-1, -2),
-                state.phones.transpose(-1, -2),
-                dim=-1,
-            ).shape,
-        )
         log.add_loss(
             "hubert_distil",
             1
             - F.cosine_similarity(
-                state.phones_prediction.transpose(-1, -2),
-                state.phones.transpose(-1, -2),
+                state.phones_prediction,
+                state.phones,
                 dim=-1,
             ).mean(),
         )
