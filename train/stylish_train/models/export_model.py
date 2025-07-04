0ca77408db418df0feb1cc25fe54a27a2d7d96d1
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-04 20:19:21 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/models/export_model.py
+++ train/stylish_train/models/export_model.py
@@ -5,9 +5,9 @@ class ExportModel(torch.nn.Module):
     def __init__(
         self,
         *,
-        text_acoustic_extractor,
+        text_hubert_distiller,
         text_duration_extractor,
-        text_spectral_extractor,
+        text_acoustic_hubert_distiller,
         duration_predictor,
         pitch_energy_predictor,
         generator,
@@ -17,9 +17,9 @@ class ExportModel(torch.nn.Module):
         super(ExportModel, self).__init__()
 
         for model in [
-            text_acoustic_extractor,
+            text_hubert_distiller,
             text_duration_extractor,
-            text_spectral_extractor,
+            text_acoustic_hubert_distiller,
             duration_predictor,
             pitch_energy_predictor,
             generator,
@@ -29,9 +29,9 @@ class ExportModel(torch.nn.Module):
                 p.requires_grad = False
 
         self.device = device
-        self.text_acoustic_extractor = text_acoustic_extractor
+        self.text_hubert_distiller = text_hubert_distiller
         self.text_duration_extractor = text_duration_extractor
-        self.text_spectral_extractor = text_spectral_extractor
+        self.text_acoustic_hubert_distiller = text_acoustic_hubert_distiller
         self.duration_predictor = duration_predictor
         self.pitch_energy_predictor = pitch_energy_predictor
         self.generator = generator
@@ -52,22 +52,35 @@ class ExportModel(torch.nn.Module):
         return pred_aln_trg
 
     def forward(self, texts, text_lengths):
-        acoustic_features, acoustic_styles = self.text_acoustic_extractor(
-            texts, text_lengths
-        )
         duration_features, _ = self.text_duration_extractor(texts, text_lengths)
-        spectral_features, spectral_styles = self.text_spectral_extractor(
-            texts, text_lengths
+        duration = self.duration_predictor(
+            duration_features,
         )
-        duration = self.duration_predictor(duration_features)
         alignment = self.duration_to_alignment(duration)
+        half_mel_lengths = torch.ones([texts.shape[0]]) * alignment.shape[1] // 2
+
+        spectral_phones, _, _ = self.text_hubert_distiller(texts, text_lengths)
+        spectral_phones = spectral_phones @ alignment
+        spectral_phones = spectral_phones.transpose(-1, -2)
+
+        acoustic_phones, _, _ = self.text_acoustic_hubert_distiller(texts, text_lengths)
+        acoustic_phones = acoustic_phones @ alignment
+        acoustic_phones = acoustic_phones.transpose(-1, -2)
+
+        acoustic_features, acoustic_styles = self.hubert_acoustic_extractor(
+            acoustic_phones, half_mel_lengths
+        )
+        spectral_features, spectral_styles = self.hubert_spectral_extractor(
+            spectral_phones, half_mel_lengths
+        )
+
         pitch, energy = self.pitch_energy_predictor(
-            spectral_features.transpose(-1, -2) @ alignment,
-            spectral_styles @ alignment,
+            spectral_features.transpose(-1, -2),
+            spectral_styles,
         )
         prediction = self.generator(
-            acoustic_features @ alignment,
-            acoustic_styles @ alignment,
+            acoustic_features,
+            acoustic_styles,
             pitch,
             energy,
         )
