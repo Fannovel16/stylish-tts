ec4ace945e3a0f5d30a034c4a35cf7cd861d9385
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-10 04:40:34 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/models/cvpl_bert.py
+++ train/stylish_train/models/cvpl_bert.py
@@ -61,9 +61,6 @@ class CVPLBERT(nn.Module):
             stochastic_sample_codes=rq_stochastic_sample_codes,
             rotation_trick=rq_rotation_trick,
             kmeans_iters=500,
-            codebook_diversity_loss_weight=0.1,
-            codebook_diversity_temperature=1.0,
-            sample_codebook_temp=1.5,
         )
         self.up = nn.Linear(hidden_dim, hubert_dim)
         self.decoder = Conformer(
