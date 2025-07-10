499a4fe4e792fe969039bf0e7e118f23f480e1eb
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-10 18:05:00 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/models/cvpl_bert.py
+++ train/stylish_train/models/cvpl_bert.py
@@ -125,8 +125,7 @@ class CVPLBERT(nn.Module):
         )
 
     def forward(self, texts, text_lengths, mel_lengths, alignment):
-        text_mask = sequence_mask(text_lengths, texts.shape[1])
         mel_mask = sequence_mask(mel_lengths, alignment.shape[2])
-        x, _, _ = self.text_encoder(texts, attention_mask=text_mask.float())
+        x, _, _ = self.text_encoder(texts, text_lengths)
         x = self.refiner((x.transpose(-1, -2) @ alignment).transpose(-1, -2), mel_mask)
         return torch.cat([head(x) for head in self.heads], dim=-1)  # BxTx(HxC)
