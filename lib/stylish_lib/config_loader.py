db8c68be37f12a3b684d923dad88b31c8faa1324
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-11 19:40:40 +0700

    Add feature distillation from HuBERT to phoneme

--- lib/stylish_lib/config_loader.py
+++ lib/stylish_lib/config_loader.py
@@ -292,7 +292,7 @@ class HubertConfig(BaseModel):
 
 
 class HubertQuantizer(BaseModel):
-    num_quantizers: int = Field(default=8, description="Number of codebooks.")
+    num_quantizers: int = Field(default=4, description="Number of codebooks.")
     codebook_size: int = Field(default=64, description="Number of rows in a codebook.")
     kmeans_init: bool = Field(
         default=True, description="Initialization with k-means clustering."
