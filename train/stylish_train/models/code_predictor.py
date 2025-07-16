e415bab43e56d12d3ec17cba4b49f38b98a743b5
H.D.Tài <tai17082006.tk@gmail.com>
2025-07-17 00:55:10 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/models/code_predictor.py
+++ train/stylish_train/models/code_predictor.py
@@ -42,6 +42,6 @@ class CodePredictor(nn.Module):
 
     def forward(self, texts, text_lengths, mel_lengths, alignment):
         mel_mask = sequence_mask(mel_lengths, alignment.shape[2])
-        x = self.text_encoder(texts, text_lengths)
+        x, _, _ = self.text_encoder(texts, text_lengths)
         x = self.refiner((x @ alignment).transpose(-1, -2), mel_mask)
         return torch.stack([head(x) for head in self.heads], dim=-2)  # BxTxHxC
