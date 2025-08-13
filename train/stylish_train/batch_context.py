83fa3b39f2798e92782b178518b193e320a77edd
H.D.Tài <tai17082006.tk@gmail.com>
2025-08-13 21:10:40 +0700

    Add feature distillation from HuBERT to phoneme

--- train/stylish_train/batch_context.py
+++ train/stylish_train/batch_context.py
@@ -53,6 +53,7 @@ class BatchContext:
 
     def acoustic_prediction_single(self, batch, use_random_mono=True):
         phones, _ = self.text_to_hubert(batch)
+        phones = phones @ batch.alignment
         acoustic_features, acoustic_styles = self.model.hubert_acoustic_extractor(
             phones, batch.mel_length // 2
         )
@@ -69,6 +70,7 @@ class BatchContext:
 
     def spectral_prediction_single(self, batch, use_random_mono=True):
         phones, _ = self.text_to_hubert(batch)
+        phones = phones @ batch.alignment
         acoustic_features, acoustic_styles = self.model.hubert_acoustic_extractor(
             phones, batch.mel_length // 2
         )
