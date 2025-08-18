2047e411d41aec255ec20ccfbc65b926b48e25d9
H.D.Tài <tai17082006.tk@gmail.com>
2025-08-18 15:25:52 +0700

    Add feature distillation from Vevo-quant of HuBERT to phoneme

--- train/stylish_train/models/vevo/vevo_utils.py
+++ train/stylish_train/models/vevo/vevo_utils.py
@@ -526,7 +526,11 @@ class VevoInferencePipeline:
             z = vqvae_model.projector(x)
             # z, _ = vqvae_model.quantizer.codebook.forward_index(z.transpose(2, 1))
             _, idx = vqvae_model.quantizer.codebook.forward_index(z.transpose(2, 1))
-            z = vqvae_model.quantizer.codebook.lookup(idx.cpu()).transpose(-1, -2)
+            z = (
+                vqvae_model.quantizer.codebook.lookup(idx.cpu())
+                .squeeze(1)
+                .transpose(-1, -2)
+            )
         else:
             raise ValueError("Invalid token_type")
 
