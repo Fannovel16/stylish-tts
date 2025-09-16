69380c6453060f65f1e8befffdff1807f199e20e
H.D.Tài <tai17082006.tk@gmail.com>
2025-09-16 19:46:52 +0700

    Mel MRD

--- src/stylish_tts/train/dataloader.py
+++ src/stylish_tts/train/dataloader.py
@@ -245,7 +245,7 @@ class Collater(object):
             alignment = self.train.duration_processor.duration_to_alignment(pred_dur)
             if self.stage != "alignment":
                 if alignment.shape[1] != mel_length:
-                    # exit(f"Alignment for segment {path} did not match audio length")
+                    exit(f"Alignment for segment {path} did not match audio length")
                     pass
                 else:
                     alignments[bid, :text_size, :mel_length] = alignment
