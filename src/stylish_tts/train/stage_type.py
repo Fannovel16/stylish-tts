794152cd01abfe30f1983aef0bb0018da1f24ce7
H.D.Tài <tai17082006.tk@gmail.com>
2025-09-22 23:32:04 +0700

    Exp with CFM XUT mel

--- src/stylish_tts/train/stage_type.py
+++ src/stylish_tts/train/stage_type.py
@@ -741,7 +741,7 @@ def validate_cfm_mel(batch, train):
         batch.pitch,
         energy,
         spk_emb,
-        n_timesteps=20,
+        n_timesteps=5,
         temperature=0.3,
     )
     audio_gt, audio_pred = train.vocos.decode(mel), train.vocos.decode(
